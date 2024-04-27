347e5df1cd7890f630f37602c625dd2695f37f47
Murilo Almeida <murilo.leite@sptech.school>
2024-04-27 10:25:36 -0300

    @MuriloAlmeid subindo depedencia mapStruct e configuração validate no jpa

--- src/main/java/sptech/elderly/config/CorsConfig.java
+++ src/main/java/sptech/elderly/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package sptech.elderly;
+package sptech.elderly.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
