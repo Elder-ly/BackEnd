d81ed1d0c10b9a410e67d8332079c5a3d6909cb9
DaniloPedrazzi <danilo.pedrazzi@sptech.school>
2024-05-31 20:21:03 -0300

    Allow all CORS

--- src/main/java/sptech/elderly/config/CorsConfig.java
+++ src/main/java/sptech/elderly/config/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://elderly.space")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
