e792b9768f4f5baf699a8c52d0112dee827db4f4
LuizSuliman <luiz.suliman@sptech.school>
2024-06-01 23:06:08 -0300

    Retorno do Endereço no ednpoint de Conversas

--- src/main/java/sptech/elderly/web/dto/mensagem/UsuarioConversaOutput.java
+++ src/main/java/sptech/elderly/web/dto/mensagem/UsuarioConversaOutput.java
@@ -4,9 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.modelmapper.ModelMapper;
 import sptech.elderly.entity.Curriculo;
 import sptech.elderly.entity.Especialidade;
+import sptech.elderly.entity.Residencia;
+import sptech.elderly.web.dto.endereco.EnderecoResumido;
 import sptech.elderly.web.dto.usuario.UsuarioMapper;
 
 import java.util.List;
@@ -17,9 +18,19 @@ public class UsuarioConversaOutput {
     private String nome;
     private String fotoPerfil;
     @JsonIgnore
+    private List<Residencia> residencias;
+    @JsonIgnore
     private List<Curriculo> curriculos;
 
     public List<Especialidade> getEspecialidades() {
         return UsuarioMapper.mapCurriculosToEspecialidades(curriculos);
+    }
+
+    public EnderecoResumido getEndereco() {
+        if (residencias.isEmpty()) return null;
+        if (residencias.get(0).getEndereco() == null) return null;
+        return new EnderecoResumido(
+                residencias.get(0).getEndereco().getBairro(),
+                residencias.get(0).getEndereco().getCidade());
     }
 }
